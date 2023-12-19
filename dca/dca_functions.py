97f649cbc793d4878d495706dfd5a8eaa2a4f0ee
Jonathan Martin <jonathanmartin@Jonathans-MBP.lan>
2023-12-19 12:18:06 -0600

    Updated hamiltonian function to allow inter-domain scoring. Added unit tests for Hamiltonian and this new functionality.

--- dca/dca_functions.py
+++ dca/dca_functions.py
@@ -1,13 +1,14 @@
-import numpy as np 
-from scipy.spatial.distance import squareform,pdist,cdist
-from numba import jit,prange
+import numpy as np
+from scipy.spatial.distance import cdist
+from numba import jit, prange
 from Bio import SeqIO
 
 # numerical sequence converter
 
-def create_numerical_MSA(sequences,alphabet):
+
+def create_numerical_MSA(sequences, alphabet):
     # if the letter is not in the alphabet, it is considered a gap.
-    seq_parser = SeqIO.parse(sequences,'fasta')
+    seq_parser = SeqIO.parse(sequences, "fasta")
     numerical_seqs = []
     headers = []
     for item in seq_parser:
@@ -17,206 +18,269 @@ def create_numerical_MSA(sequences,alphabet):
             try:
                 newseq.append(alphabet[letter])
             except:
-                newseq.append(alphabet['-'])
+                newseq.append(alphabet["-"])
         numerical_seqs.append(newseq)
-    numerical_seqs = np.array(numerical_seqs,dtype=int)
+    numerical_seqs = np.array(numerical_seqs, dtype=int)
     return numerical_seqs, headers
+
 
 # loading couplings from Mi3GPU, compute gaugeless DI
 
+
 def load_couplings(N, q, eij_file):
-    if '.npy' in eij_file:
+    if ".npy" in eij_file:
         couplings = np.load(eij_file)
     else:
         couplings = np.loadtxt(eij_file)
     # check that the input is correct
-    if couplings.shape[1] == q*q:
+    if couplings.shape[1] == q * q:
         pass
-    elif couplings.shape[0] == q*q:
+    elif couplings.shape[0] == q * q:
         couplings = np.transpose(couplings)
     else:
-        exit('The couplings do not have a dim of q*q (q*q ij block, flattened). Please reformat your data!')
-    
-    formatted_couplings = np.zeros((N,N,q,q))
+        exit(
+            "The couplings do not have a dim of q*q (q*q ij block, flattened). Please reformat your data!"
+        )
+
+    formatted_couplings = np.zeros((N, N, q, q))
     count = 0
-    for i in range(N-1):
-        for j in range(i+1,N):
-            formatted_couplings[i,j,:,:] = np.reshape(couplings[count],(q,q))
-            formatted_couplings[j,i,:,:] = formatted_couplings[i,j,:,:]
+    for i in range(N - 1):
+        for j in range(i + 1, N):
+            formatted_couplings[i, j, :, :] = np.reshape(couplings[count], (q, q))
+            formatted_couplings[j, i, :, :] = formatted_couplings[i, j, :, :]
             count += 1
     return formatted_couplings
 
-def compute_DI_justcouplings(N,q,couplings):
-    Pdir = np.zeros((N,N,q,q))
-    DI = np.zeros( ( int(N*(N-1)/2) , 3 ) )
+
+def compute_DI_justcouplings(N, q, couplings):
+    Pdir = np.zeros((N, N, q, q))
+    DI = np.zeros((int(N * (N - 1) / 2), 3))
     count = 0
-    for i in range(N-1):
-        for j in range(i+1,N):
-            pdir_unnormalized = np.exp(couplings[i,j,:,:])
+    for i in range(N - 1):
+        for j in range(i + 1, N):
+            pdir_unnormalized = np.exp(couplings[i, j, :, :])
             Pab = pdir_unnormalized / pdir_unnormalized.sum()
-            fa = np.sum(Pab,1)
-            fb = np.sum(Pab,0)
-            temp = np.zeros((q,q))
+            fa = np.sum(Pab, 1)
+            fb = np.sum(Pab, 0)
+            temp = np.zeros((q, q))
             for qi in range(q):
                 for qj in range(q):
-                    temp[qi,qj] = (Pab[qi,qj] / ( fa[qi] * fb[qj] ))
+                    temp[qi, qj] = Pab[qi, qj] / (fa[qi] * fb[qj])
             temp = np.log(temp)
-            DI[count,:] = [i,j,np.trace( np.dot(Pab.conj().T,temp) )]
+            DI[count, :] = [i, j, np.trace(np.dot(Pab.conj().T, temp))]
             count += 1
     return DI
 
+
 # functions for mfDCA
 
-def compute_W(sequences,theta,batch_size):
-    '''handles very large data so that it fits into memory'''
+
+def compute_W(sequences, theta, batch_size):
+    """handles very large data so that it fits into memory"""
     msa_len = sequences.shape[0]
     seq_len = sequences.shape[1]
-    output_W = np.zeros(msa_len,dtype=np.float64)
+    output_W = np.zeros(msa_len, dtype=np.float64)
     for idx in range(0, msa_len, batch_size):
-        if idx+batch_size > msa_len:
-            computation = (cdist(sequences[idx:], sequences, 'hamm') < theta).sum(1)
+        if idx + batch_size > msa_len:
+            computation = (cdist(sequences[idx:], sequences, "hamm") < theta).sum(1)
             output_W[idx:] = computation
         else:
-            computation = ( (cdist(sequences[idx:idx+batch_size],sequences,'hamm') < theta).sum(1) )
-            output_W[idx:idx+batch_size] = computation
+            computation = (
+                cdist(sequences[idx : idx + batch_size], sequences, "hamm") < theta
+            ).sum(1)
+            output_W[idx : idx + batch_size] = computation
     return 1 / output_W
 
 
 @jit(nopython=True)
-def compute_Pi(sequences, pseudocount, N, M, q, Meff, W): #count single site statistics
-    '''np.array((N,N,q,q)), returns non-diagonal Pij matrix'''
-    Pi = np.zeros((N,q),dtype=np.float64)
-    for j in range(N):  
+def compute_Pi(
+    sequences, pseudocount, N, M, q, Meff, W
+):  # count single site statistics
+    """np.array((N,N,q,q)), returns non-diagonal Pij matrix"""
+    Pi = np.zeros((N, q), dtype=np.float64)
+    for j in range(N):
         for l in range(M):
-            Pi[j,sequences[l,j]] = Pi[j,sequences[l,j]] + W[l]
-    
+            Pi[j, sequences[l, j]] = Pi[j, sequences[l, j]] + W[l]
+
     Pi = Pi / Meff
 
     return Pi
 
-@jit(nopython=True,parallel=True)
-def compute_Pij(sequences, pseudocount, N, M, q, Meff, W, Pi): # count pairwise statistics
-    Pij = np.zeros((N,N,q,q),dtype=np.float64)
-    for i in prange(N-1):
-        for j in range(i+1,N):
+
+@jit(nopython=True, parallel=True)
+def compute_Pij(
+    sequences, pseudocount, N, M, q, Meff, W, Pi
+):  # count pairwise statistics
+    Pij = np.zeros((N, N, q, q), dtype=np.float64)
+    for i in prange(N - 1):
+        for j in range(i + 1, N):
             for l in range(M):
-                Pij[i,j,sequences[l,i],sequences[l,j]] = Pij[i,j,sequences[l,i],sequences[l,j]] + W[l]
-                Pij[j,i,sequences[l,j],sequences[l,i]] = Pij[i,j,sequences[l,i],sequences[l,j]]
-    
+                Pij[i, j, sequences[l, i], sequences[l, j]] = (
+                    Pij[i, j, sequences[l, i], sequences[l, j]] + W[l]
+                )
+                Pij[j, i, sequences[l, j], sequences[l, i]] = Pij[
+                    i, j, sequences[l, i], sequences[l, j]
+                ]
+
     Pij = Pij / Meff
 
-    qq_id = np.eye(q,q,dtype=np.float64)
+    qq_id = np.eye(q, q, dtype=np.float64)
     for i in range(N):
         for alpha in range(q):
             for beta in range(q):
-                    Pij[i,i,alpha,beta] = Pi[i,alpha] * qq_id[alpha,beta]
+                Pij[i, i, alpha, beta] = Pi[i, alpha] * qq_id[alpha, beta]
     return Pij
 
-@jit(nopython=True)
-def add_pseudocount(Pi,Pij,pseudocount_weight,N,q): #add pseudocount to counting matrices 
-    Pij_pc = (1 - pseudocount_weight) * Pij +  pseudocount_weight / q / q * np.ones((N,N,q,q),dtype=np.float64)
-    Pi_pc = (1 - pseudocount_weight) * Pi + pseudocount_weight / q * np.ones((N,q),dtype=np.float64)
 
-    qq_id = np.eye(q,q,dtype=np.float64)
+@jit(nopython=True)
+def add_pseudocount(
+    Pi, Pij, pseudocount_weight, N, q
+):  # add pseudocount to counting matrices
+    Pij_pc = (1 - pseudocount_weight) * Pij + pseudocount_weight / q / q * np.ones(
+        (N, N, q, q), dtype=np.float64
+    )
+    Pi_pc = (1 - pseudocount_weight) * Pi + pseudocount_weight / q * np.ones(
+        (N, q), dtype=np.float64
+    )
+
+    qq_id = np.eye(q, q, dtype=np.float64)
 
     for i in range(N):
         for alpha in range(q):
             for beta in range(q):
-                    Pij_pc[i,i,alpha,beta] = (1-pseudocount_weight)*Pij[i,i,alpha,beta] + pseudocount_weight / q * qq_id[alpha,beta]
+                Pij_pc[i, i, alpha, beta] = (1 - pseudocount_weight) * Pij[
+                    i, i, alpha, beta
+                ] + pseudocount_weight / q * qq_id[alpha, beta]
     return Pi_pc, Pij_pc
 
+
 @jit(nopython=True)
-def computeC(Pi,Pij,N,q): # produce connected correlation matrix
-    C=np.zeros((N*(q-1),N*(q-1)),dtype=np.float64)
+def computeC(Pi, Pij, N, q):  # produce connected correlation matrix
+    C = np.zeros((N * (q - 1), N * (q - 1)), dtype=np.float64)
     for i in range(N):
         for j in range(N):
-            for alpha in range(q-1):
-                for beta in range(q-1):
-                     C[(q-1)*(i)+alpha,(q-1)*(j)+beta] = Pij[i,j,alpha,beta] - Pi[i,alpha]*Pi[j,beta]
+            for alpha in range(q - 1):
+                for beta in range(q - 1):
+                    C[(q - 1) * (i) + alpha, (q - 1) * (j) + beta] = (
+                        Pij[i, j, alpha, beta] - Pi[i, alpha] * Pi[j, beta]
+                    )
     return C
 
-def invC_to_4D(invC, N, q): # create 4D eij array from inverted C matrix
-    reshaped_invC = np.zeros((N,N,q,q),dtype=np.float64)
+
+def invC_to_4D(invC, N, q):  # create 4D eij array from inverted C matrix
+    reshaped_invC = np.zeros((N, N, q, q), dtype=np.float64)
     for i in range(N):
         for j in range(N):
-            reshaped_invC[i,j,:,:] = np.pad( invC[ i * (q-1) : (i+1) * (q-1), 
-                                            j * (q-1) : (j) * (q-1) + (q - 1) ] , 
-                                            ((0,1),(0,1)), mode='constant' )
+            reshaped_invC[i, j, :, :] = np.pad(
+                invC[
+                    i * (q - 1) : (i + 1) * (q - 1),
+                    j * (q - 1) : (j) * (q - 1) + (q - 1),
+                ],
+                ((0, 1), (0, 1)),
+                mode="constant",
+            )
     return reshaped_invC
 
+
 @jit(nopython=True)
-def Compute_Results(Pi, invC, N, q): # calculate DI score for each pair of positions
-    Pairwisehfield = np.zeros((N*q,2*N),dtype=np.float64)
-    DI_pairs = np.zeros((int(N*(N-1)/2),3),dtype=np.float64)
+def Compute_Results(Pi, invC, N, q):  # calculate DI score for each pair of positions
+    Pairwisehfield = np.zeros((N * q, 2 * N), dtype=np.float64)
+    DI_pairs = np.zeros((int(N * (N - 1) / 2), 3), dtype=np.float64)
     epsilon = 0.00004
     count = 0
-    tiny = np.nextafter(0,1)
+    tiny = np.nextafter(0, 1)
 
     for i in range(N):
-        for j in range(i+1,N):
-            #direct information from mean-field
-            W_mf = np.ones((q,q),dtype=np.float64)
-            W_mf[:q-1, :q-1] = np.exp( - invC[ i, j, :q-1, :q-1 ] )
+        for j in range(i + 1, N):
+            # direct information from mean-field
+            W_mf = np.ones((q, q), dtype=np.float64)
+            W_mf[: q - 1, : q - 1] = np.exp(-invC[i, j, : q - 1, : q - 1])
             diff = 1.0
-            mu1 = np.ones(q,dtype=np.float64) / q; mu2 = np.ones(q,dtype=np.float64) / q
-            pi = Pi[i,:] ; pj = Pi[j,:]
+            mu1 = np.ones(q, dtype=np.float64) / q
+            mu2 = np.ones(q, dtype=np.float64) / q
+            pi = Pi[i, :]
+            pj = Pi[j, :]
             while diff > epsilon:
                 calc1 = np.dot(mu2, W_mf.T)
-                calc2 = np.dot(mu1,  W_mf)
+                calc2 = np.dot(mu1, W_mf)
 
-                new1 = np.divide(pi,calc1)
+                new1 = np.divide(pi, calc1)
                 new1 = new1 / new1.sum()
-                new1_diff = np.absolute( new1 - mu1)
+                new1_diff = np.absolute(new1 - mu1)
 
-                new2 = np.divide(pj,calc2)
+                new2 = np.divide(pj, calc2)
                 new2 = new2 / new2.sum()
-                new2_diff = np.absolute( new2 - mu2)
+                new2_diff = np.absolute(new2 - mu2)
 
-                diff = np.maximum( new1_diff, new2_diff).max()
-                mu1 = new1; mu2 = new2
+                diff = np.maximum(new1_diff, new2_diff).max()
+                mu1 = new1
+                mu2 = new2
 
-            #DI pair calc
-            mu1_t = np.expand_dims(mu1,axis=1)
+            # DI pair calc
+            mu1_t = np.expand_dims(mu1, axis=1)
             Pdir = W_mf * (mu1_t * mu2)
             Pdir = Pdir / Pdir.sum()
 
             Pi_t = np.expand_dims(pi, axis=1)
             Pfac = Pi_t * pj
-            DI = np.trace( np.dot( Pdir.T, np.log( (Pdir+tiny) / (Pfac+tiny) ) ) ) 
-            DI_pairs[count, : ] = np.array([i, j, DI])
+            DI = np.trace(np.dot(Pdir.T, np.log((Pdir + tiny) / (Pfac + tiny))))
+            DI_pairs[count, :] = np.array([i, j, DI])
             count += 1
 
-            #pairwise fields
+            # pairwise fields
             mu1 = np.log(mu1 / mu1[-1])
             mu2 = np.log(mu2 / mu2[-1])
-            hihj = np.vstack((mu1,mu2)).T
-            Pairwisehfield[(i)*q:(i*q)+q , 2*j:2*j+2 ] = hihj
-            Pairwisehfield[(j)*q:(j*q)+q , 2*i:2*i+2 ] = hihj[:, ::-1]
+            hihj = np.vstack((mu1, mu2)).T
+            Pairwisehfield[(i) * q : (i * q) + q, 2 * j : 2 * j + 2] = hihj
+            Pairwisehfield[(j) * q : (j * q) + q, 2 * i : 2 * i + 2] = hihj[:, ::-1]
     return Pairwisehfield, DI_pairs
 
-def Compute_AverageLocalField(Pairwisehfield,N,q): # caluclate local fields (h)
-    hi = np.zeros((q,N),dtype=np.float64)
+
+def Compute_AverageLocalField(Pairwisehfield, N, q):  # caluclate local fields (h)
+    hi = np.zeros((q, N), dtype=np.float64)
     # average i->rest pairwise fields, returns 21*N matrix
     # 1st,3rd,5th...2*N-1 columns, excluding the column at pos i.
     # split the matrix at these intervals, pull i+i*21 rows and average these N-1 values together. Append to position i.
     for i in prange(N):
-        pairwise_chunk = ( Pairwisehfield[ i*q: (i*q)+q , range(0,N*2,2) ].sum(1) - Pairwisehfield[ i*q: (i*q)+q , i ] ) / ( N - 1 )
-        hi[:,i] = pairwise_chunk
-    return hi 
+        pairwise_chunk = (
+            Pairwisehfield[i * q : (i * q) + q, range(0, N * 2, 2)].sum(1)
+            - Pairwisehfield[i * q : (i * q) + q, i]
+        ) / (N - 1)
+        hi[:, i] = pairwise_chunk
+    return hi
+
 
 @jit(nopython=True)
-def return_Hamiltonian(numerical_sequences, couplings, localfields, N1=""): # calculates Hamiltonian for input fasta
+def return_Hamiltonian(
+    numerical_sequences, couplings, localfields, interDomainCutoff=None
+):  # calculates Hamiltonian for input fasta
     M = numerical_sequences.shape[0]
     L = numerical_sequences.shape[1]
-    hamiltonians = np.zeros(M,dtype=np.float64) 
+    hamiltonians = np.zeros(M, dtype=np.float64)
 
-    for l in range(M):
+    first, second = np.triu_indices(couplings.shape[0], k=1)
+    pairs = np.stack((first, second), axis=1)
+
+    if isinstance(interDomainCutoff, int):
+        pairs = pairs[
+            (pairs[:, 0] < interDomainCutoff) & (pairs[:, 1] >= interDomainCutoff)
+        ]
+
+    for seq in range(M):
         for i in range(L):
-            hamiltonians[l] = hamiltonians[l] + localfields[numerical_sequences[l, i], i ]
-    for l in range(M):
-        for i in range(L-1):
-            for j in range(i+1,L):
-                hamiltonians[l] = hamiltonians[l] + couplings[ i , j , numerical_sequences[l,i] , numerical_sequences[l,j] ] 
-    
+            hamiltonians[seq] = (
+                hamiltonians[seq] + localfields[numerical_sequences[seq, i], i]
+            )
+    for seq in range(M):
+        for pair in pairs:
+            hamiltonians[seq] = (
+                hamiltonians[seq]
+                + couplings[
+                    pair[0],
+                    pair[1],
+                    numerical_sequences[seq, pair[0]],
+                    numerical_sequences[seq, pair[1]],
+                ]
+            )
+
     return -hamiltonians
-    
--- unit_testing/readme.md
+++ unit_testing/readme.md
@@ -12,38 +12,47 @@ Reasons for discrepencies:
 - Spearmans correlation of all DI pairs is 98.5%.
 
 **Update March 8, 2022. Same results as below with new compute_W function.**
-
+**Update December 19, 2023. Added test for Hamiltonians and inter-domain hamiltonians.**
 
 ```shell
 (dca) unit_testing.py
-test_check_header_accuracy (__main__.test_NumericalMSA) ... ok
-test_check_sequence_conversion (__main__.test_NumericalMSA) ... ok
-test_DI_calculation_closeness (__main__.test_computeDI_justcouplings) ... ok
-test_C_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_DI_arraysize (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_DI_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI) ... FAIL
-test_equivalent_reweighted_sequences (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_invC_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI) ... FAIL
-test_pi_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_pij_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_pseudocount_pi (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_pseudocount_pij (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_spearmans_DI_100 (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_spearmans_DI_1000 (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_spearmans_DI_50 (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_spearmans_DI_500 (__main__.test_compute_Pi_Pij_pseudocount_DI) ... ok
-test_check_correct_dimensions (__main__.test_load_couplings) ... ok
-test_correct_filling (__main__.test_load_couplings) ... ok
+test_check_header_accuracy (__main__.test_NumericalMSA.test_check_header_accuracy) ... ok
+test_check_sequence_conversion (__main__.test_NumericalMSA.test_check_sequence_conversion) ... ok
+test_DI_calculation_closeness (__main__.test_computeDI_justcouplings.test_DI_calculation_closeness) ... ok
+/Users/_/code/eil/py-mfdca/dca/dca_functions.py:264: NumbaExperimentalFeatureWarning: Use of isinstance() detected. This is an experimental feature.
+  if isinstance(interDomainCutoff, int):
+/Users/_/code/eil/py-mfdca/dca/dca_functions.py:264: NumbaExperimentalFeatureWarning: Use of isinstance() detected. This is an experimental feature.
+  if isinstance(interDomainCutoff, int):
+test_C_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_C_closeness) ... ok
+test_DI_arraysize (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_DI_arraysize) ... ok
+test_DI_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_DI_closeness) ... FAIL
+test_domain_hamiltonians (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_domain_hamiltonians) ... ok
+test_equivalent_reweighted_sequences (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_equivalent_reweighted_sequences) ... ok
+test_full_hamiltonians (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_full_hamiltonians) ... ok
+test_invC_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_invC_closeness) ... FAIL
+test_pi_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_pi_closeness) ... ok
+test_pij_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_pij_closeness) ... ok
+test_pseudocount_pi (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_pseudocount_pi) ... ok
+test_pseudocount_pij (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_pseudocount_pij) ... ok
+test_spearmans_DI_100 (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_spearmans_DI_100) ... ok
+test_spearmans_DI_1000 (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_spearmans_DI_1000) ... ok
+test_spearmans_DI_50 (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_spearmans_DI_50) ... ok
+test_spearmans_DI_500 (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_spearmans_DI_500) ... ok
+test_check_correct_dimensions (__main__.test_load_couplings.test_check_correct_dimensions) ... ok
+test_correct_filling (__main__.test_load_couplings.test_correct_filling) ... ok
 
 ======================================================================
-FAIL: test_DI_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI)
+FAIL: test_DI_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_DI_closeness)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File "dca/unit_testing.py", line 89, in test_DI_closeness
-    np.testing.assert_allclose(self.calc_di[:,2],self.loaded_di[:,2])
-  File "miniconda/base/envs/dca/lib/python3.9/site-packages/numpy/testing/_private/utils.py", line 1528, in assert_allclose
+  File "/Users/_/code/eil/py-mfdca/unit_testing.py", line 166, in test_DI_closeness
+    np.testing.assert_allclose(self.calc_di[:, 2], self.loaded_di[:, 2])
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/site-packages/numpy/testing/_private/utils.py", line 1592, in assert_allclose
     assert_array_compare(compare, actual, desired, err_msg=str(err_msg),
-  File "miniconda/base/envs/dca/lib/python3.9/site-packages/numpy/testing/_private/utils.py", line 842, in assert_array_compare
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/contextlib.py", line 81, in inner
+    return func(*args, **kwds)
+           ^^^^^^^^^^^^^^^^^^^
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/site-packages/numpy/testing/_private/utils.py", line 862, in assert_array_compare
     raise AssertionError(msg)
 AssertionError: 
 Not equal to tolerance rtol=1e-07, atol=0
@@ -55,29 +64,32 @@ Max relative difference: 0.00971624
  y: array([0.088411, 0.018345, 0.013299, ..., 0.325342, 0.025643, 0.079542])
 
 ======================================================================
-FAIL: test_invC_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI)
+FAIL: test_invC_closeness (__main__.test_compute_Pi_Pij_pseudocount_DI_Hamiltonians.test_invC_closeness)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File "dca/unit_testing.py", line 85, in test_invC_closeness
-    np.testing.assert_allclose(self.invC,self.invC_matlab)
-  File "miniconda/base/envs/dca/lib/python3.9/site-packages/numpy/testing/_private/utils.py", line 1528, in assert_allclose
+  File "/Users/_/code/eil/py-mfdca/unit_testing.py", line 160, in test_invC_closeness
+    np.testing.assert_allclose(self.invC, self.invC_matlab)
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/site-packages/numpy/testing/_private/utils.py", line 1592, in assert_allclose
     assert_array_compare(compare, actual, desired, err_msg=str(err_msg),
-  File "miniconda/base/envs/dca/lib/python3.9/site-packages/numpy/testing/_private/utils.py", line 842, in assert_array_compare
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/contextlib.py", line 81, in inner
+    return func(*args, **kwds)
+           ^^^^^^^^^^^^^^^^^^^
+  File "/Users/_/Code/miniconda3/envs/pytorch/lib/python3.11/site-packages/numpy/testing/_private/utils.py", line 862, in assert_array_compare
     raise AssertionError(msg)
 AssertionError: 
 Not equal to tolerance rtol=1e-07, atol=0
 
-Mismatched elements: 181749 / 2689600 (6.76%)
-Max absolute difference: 1.40259779e-11
-Max relative difference: 384275.22300591
- x: array([[ 7.098221e+01,  4.200000e+01,  4.200000e+01, ..., -3.923955e-11,
-        -3.924689e-11, -1.184982e-02],
-       [ 4.200000e+01,  8.400000e+01,  4.200000e+01, ..., -3.845147e-11,...
+Mismatched elements: 181763 / 2689600 (6.76%)
+Max absolute difference: 1.38114398e-11
+Max relative difference: 384117.42763853
+ x: array([[ 7.098221e+01,  4.200000e+01,  4.200000e+01, ..., -3.921236e-11,
+        -3.920969e-11, -1.184982e-02],
+       [ 4.200000e+01,  8.400000e+01,  4.200000e+01, ..., -3.840802e-11,...
  y: array([[ 7.098221e+01,  4.200000e+01,  4.200000e+01, ..., -2.842980e-11,
         -2.842981e-11, -1.184982e-02],
        [ 4.200000e+01,  8.400000e+01,  4.200000e+01, ..., -2.506304e-11,...
 
 ----------------------------------------------------------------------
-Ran 18 tests in 147.081s
+Ran 20 tests in 80.795s
 
 FAILED (failures=2)
